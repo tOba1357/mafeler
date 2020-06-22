c640f7cdee41b2453a264a1710c68f372c4b74d9
Tatsuya Oba <toba17893@gmail.com>
2020-06-22 23:29:26 +0900

    fix

--- app/controllers/fitbit/webhook_controller.rb
+++ app/controllers/fitbit/webhook_controller.rb
@@ -1,5 +1,6 @@
 module Fitbit
   class WebhookController < Fitbit::ApplicationController
+    skip_before_action :associate_fitbit_account
     # https://dev.fitbit.com/build/reference/web-api/subscriptions/#verify-a-subscriber
     def index
       if params[:verify] == ENV['FITIBT_VERIFICATION_CODE']
